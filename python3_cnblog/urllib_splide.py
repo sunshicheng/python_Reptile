2b5b46af77005eaadbd0246d34026b4f176177ce
sunshicheng <1248185022@qq.com>
2019-12-02 22:12:24 +0800

    up 知识点

--- python3_cnblog/urllib_splide.py
+++ python3_cnblog/urllib_splide.py
@@ -8,9 +8,7 @@ FileName : urllib_splide.py
 """
 
 """
-使用urlopen函数打开网页
-
-
+#urlopen 函数
 
 import urllib.request
 
@@ -21,7 +19,8 @@ print(response.read())
 print(response.status)
 print(response.getheaders())
 print(response.getheader('Server'))
-"""
+
+#data()
 
 import urllib.parse
 import urllib.request
@@ -31,3 +30,30 @@ ssl._create_default_https_context = ssl._create_unverified_context
 data = bytes(urllib.parse.urlencode({'name':'hello','pass':'123'}),encoding='utf-8')
 response = urllib.request.urlopen('http://www.iqianyue.com/mypost',data=data)
 print(response.read())
+
+
+#timeout
+
+import urllib.request
+# response = urllib.request.urlopen('http://www.python.org',timeout=1)
+# print(response.text)
+
+import socket
+import urllib.request
+import urllib.error
+
+try:
+    response = urllib.request.urlopen('http://httpbin.org/get',timeout=1)
+except urllib.error.URLError as e:
+    if isinstance(e.reason,socket.timeout):
+        print('TIME OUT')
+"""
+
+import urllib.request
+
+request = urllib.request.Request("http://www.baidu.com")
+
+response = urllib.request.urlopen(request)
+
+print(response.read().decode('utf-8'))
+
